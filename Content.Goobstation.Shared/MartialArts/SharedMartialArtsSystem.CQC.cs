537b8aa5e9c7f6fbe885c91cd2da1bd54233bac6
NotActuallyMarty <27968892+NotActuallyMarty@users.noreply.github.com>
2026-02-12 22:50:13 +0200

    Fix CQC legsweep not standing you. (#6037)
    sweep

--- Content.Goobstation.Shared/MartialArts/SharedMartialArtsSystem.CQC.cs
+++ Content.Goobstation.Shared/MartialArts/SharedMartialArtsSystem.CQC.cs
@@ -38,6 +38,7 @@ using Content.Shared.Mobs.Components;
 using Content.Shared.Movement.Pulling.Components;
 using Content.Shared.Standing;
 using Content.Shared.Weapons.Melee; //omu
+using Content.Shared.Stunnable;
 using Robust.Shared.Audio;
 using Robust.Shared.Utility;
 
@@ -145,14 +146,16 @@ public partial class SharedMartialArtsSystem
             case ComboAttackType.Harm:
 
                 // Leg sweep
-                 if (!TryComp<StandingStateComponent>(ent, out var standing)
+                 if (!TryComp<StandingStateComponent>(ent.Owner, out var standing)
                      || standing.Standing
-                     || !TryComp(args.Target, out StandingStateComponent? targetStanding) ||
-                     !targetStanding.Standing)
+                     || !TryComp<StandingStateComponent>(args.Target, out var targetStanding)
+                     || !targetStanding.Standing
+                     )
                      break;
+                if (HasComp<KnockedDownComponent>(ent.Owner))
+                    RemComp<KnockedDownComponent>(ent.Owner);
 
-                _status.TryRemoveStatusEffect(ent, "KnockedDown");
-                _standingState.Stand(ent);
+                _standingState.Stand(ent.Owner);
                 _stun.TryKnockdown(args.Target, TimeSpan.FromSeconds(5), true);
                 ComboPopup(ent, args.Target, "Leg Sweep");
                 break;
